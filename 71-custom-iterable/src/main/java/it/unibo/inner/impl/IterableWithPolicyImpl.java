6ee3813c00ba1ad2c096c23d7c1cbec0aa878b15
Mattia Ronchi - mattia.ronchi5@studio.unibo.it <mattia.ronchi5@studio.unibo.it>
2023-11-07 16:24:05 +0100

    second part of 71

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -1,6 +1,7 @@
 package it.unibo.inner.impl;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import it.unibo.inner.api.IterableWithPolicy;
@@ -8,14 +9,27 @@ import it.unibo.inner.api.Predicate;
 
 public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
 
-    private T[] arr;
+    private final List<T> elements;
+    private Predicate<T> filter;
 
-    public IterableWithPolicyImpl(final T[] arr){
-        this.arr = arr;
+    public IterableWithPolicyImpl(final T[] elements){
+        this(
+            elements,
+            new Predicate<T>(){
+                public boolean test(final T elem) {
+                    return true;    
+                }            
+            }
+        );
+    }
+
+    public IterableWithPolicyImpl(final T[] elements, final Predicate<T> filter){
+        this.elements = List.of(elements);
+        this.setIterationPolicy(filter);
     }
     
     public void setIterationPolicy(final Predicate<T> filter){
-
+        this.filter = filter;
     }
 
     public Iterator<T> iterator(){
@@ -27,18 +41,26 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
         private int index = 0;
 
         public boolean hasNext() {
-            return index < IterableWithPolicyImpl.this.arr.length;
+            while(isIndexValid() && !isValidElement()){
+                index++;
+            }
+            return isIndexValid();
         }
 
         public T next() {
             if(!this.hasNext()){
                 throw new NoSuchElementException();
             }
-
-            T elem = IterableWithPolicyImpl.this.arr[index];
-            index++;
-            return elem;
+            return elements.get(index++);
         }
-        
+
+        private boolean isIndexValid(){
+            return index >= 0 && index < elements.size();
+        }
+
+        private boolean isValidElement(){
+            return filter.test(elements.get(index));
+        }
+
     }
 }
